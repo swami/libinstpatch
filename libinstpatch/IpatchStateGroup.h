3efee9b7d3d5da770ce39ae79ae4eb665389510b
derselbst <tom.mbrt@googlemail.com>
2019-12-03 15:34:15 +0100

    Fix inconsistent licensing in file headers

--- libinstpatch/IpatchStateGroup.h
+++ libinstpatch/IpatchStateGroup.h
@@ -5,7 +5,7 @@
  * Copyright (C) 1999-2014 Element Green <element@elementsofsound.org>
  *
  * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
+ * modify it under the terms of the GNU Lesser General Public License
  * as published by the Free Software Foundation; version 2.1
  * of the License only.
  *
