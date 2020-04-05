7b3602f0b759779e2cab8a202412ecceebd7c21b
jjceresa <jjc_fluid@orange.fr>
2020-04-05 19:21:53 +0200

    Revert change and add comment

--- libinstpatch/IpatchConverter.c
+++ libinstpatch/IpatchConverter.c
@@ -216,7 +216,12 @@ ipatch_convert_object_to_type_multi(GObject *object, GType type, GError **err)
 GList *
 ipatch_convert_object_to_type_multi_list(GObject *object, GType type, GError **err)
 {
-    return (ipatch_convert_object_to_type_multi_set_vlist(object, type, err, NULL, NULL));
+    /* Note: empty is intentionally not initialized. It allows to fix some on ARM and PPC.
+       It is save to use, because empty is not accessed in ipatch_convert_object_to_type_multi_set_vlist
+       when the second last argument is NULL.
+    */
+    va_list empty;
+    return (ipatch_convert_object_to_type_multi_set_vlist(object, type, err, NULL, empty));
 }
 
 /**
